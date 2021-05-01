aa041d120f6cc710cc2524c9bcbc175d886a6a9d
SebastianSafaiRad <60371753+SebastianSafaiRad@users.noreply.github.com>
2021-05-01 16:13:44 -0600

    updated build method

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,6 @@
 from graph import Graph
-import queue2050
+from graph import Vertex
+from queue2050 import Queue
 
 
 class assignment6:
@@ -12,26 +13,50 @@ class assignment6:
 
     def findsolution(self, a, b, goal_amount):
         return self.bfSearch(self.buildGraph())
-    
 
     def getEligibleStates(self, a, b, curr_state):
         pass
 
     def buildGraph(self):
-        curr_node = self.graph.addVertex(self.getContainerContents())
-        done = False
-        while not done:
-            pass
+        q = Queue
+        q.enqueue(self.graph.addVertex("0,0"))
+        while not q.isEmpty():
+            curr_node = q.dequeue()
+            nodeid = curr_node.getId()
+            idList = nodeid.split(",")
+            self.container_a.setCurrVolume(int(idList[0]))
+            self.container_b.setCurrVolume(int(idList[1]))
+            if not self.container_a.isFull():
+                self.container_a.fill()
+                newVertex = Vertex(self.container_a.getCurrVolume() + "," + idList[1])
+                self.graph.addEdge(curr_node, newVertex)
+                q.enqueue(newVertex)
+                self.container_a.setCurrVolume(idList[0])
+            if not self.container_b.isFull():
+                self.container_b.fill()
+                newVertex = Vertex(idList[0] + "," + self.container_b.getCurrVolume())
+                self.graph.addEdge(curr_node, newVertex)
+                q.enqueue(newVertex)
+                self.container_b.setCurrVolume(idList[1])
+            if self.canPour(self.container_a, self.container_b):
 
 
-    def getContainerContents(self):
-        return self.container_a.getCurrVolume() + "," + self.container_b.getCurrVolume()
+
 
     def bfSearch(self, graph):
         pass
 
-    def pour(self, a, b):
-        pass
+    def pour(self, source, destination):
+        
+
+
+    def canPour(self, source, destination):
+        if source.isEmpty():
+            return False
+        elif destination.isFull():
+            return False
+        else:
+            return True
 
 
 class Container:
@@ -44,16 +69,29 @@ class Container:
     def fill(self):
         self.curr_volume = self.max_volume
 
-
     def getCurrVolume(self):
         return self.curr_volume
 
     def getMaxVolume(self):
         return self.max_volume
 
+    def setCurrVolume(self, volume):
+        self.v = volume
+
+    def isFull(self):
+        if self.curr_volume == self.max_volume:
+            return True
+        else:
+            return False
+
+    def isEmpty(self):
+        if self.curr_volume == 0:
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
-
     g = Graph()
     node = g.addVertex("0,0")
 
