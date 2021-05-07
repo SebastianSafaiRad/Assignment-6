4b88a23b5ed35f4038404977449b92bacba4c053
SebastianSafaiRad <60371753+SebastianSafaiRad@users.noreply.github.com>
2021-05-07 17:35:14 -0600

    finished test methods

--- assignment6.py
+++ assignment6.py
@@ -2,12 +2,13 @@ from graph import Graph
 from graph import Vertex
 from queue2050 import Queue
 import math
+import unittest
 
 
 class assignment6:
 
     def __init__(self):
-        """Constructor for this assignment"""
+        """Constructor for this class"""
         self.container_a = None
         self.container_b = None
         self.goal_amount = None
@@ -16,6 +17,8 @@ class assignment6:
         self.q = Queue()
 
     def findsolution(self, a, b, goal_amount):
+        """Takes input of amount to ge store in container a and container b and the goal amount.
+        Then it prints out the solution path"""
         self.container_a = Container(a)
         self.container_b = Container(b)
         self.goal_amount = str(goal_amount)
@@ -26,6 +29,8 @@ class assignment6:
             print("There is no solution")
 
     def getEligibleStates(self, a, b, curr_state):
+        """Takes input of amount to store in container a and container b.  Takes the current state
+        and prints out all of the possible states from the current state"""
         self.container_a = Container(a)
         self.container_b = Container(b)
         self.rootNodeState = curr_state
@@ -33,7 +38,7 @@ class assignment6:
         stateList = []
         for vertex in self.graph.getVertices():
             stateList.append(vertex)
-            #print(vertex)
+            # print(vertex)
         return stateList
 
     def buildGraph2(self):
@@ -63,7 +68,7 @@ class assignment6:
                 self.addToQueue(self.addEdge(parentStateString, self.getStateString()))
                 self.resetContainers(parentStateString)
 
-        #self.printGraph()
+        # self.printGraph()
 
     def resetContainers(self, containersState):
         # containerState is a two-element csv string.  e.g. "0,3"
@@ -100,6 +105,7 @@ class assignment6:
             self.q.enqueue(newVertex)
 
     def bfSearch(self):
+        """Performs a Breatdh First Search"""
         queue = Queue()
         rootVertex = self.graph.getVertex(self.rootNodeState)
         queue.enqueue(rootVertex)
@@ -116,6 +122,7 @@ class assignment6:
         print("Solution not found")
 
     def pour(self, source, destination):
+        """"""
         maxAmountToPour = destination.max_volume - int(destination.curr_volume)
         if source.curr_volume <= maxAmountToPour:
             destination.curr_volume = destination.curr_volume + source.curr_volume
@@ -125,6 +132,7 @@ class assignment6:
             destination.curr_volume = destination.curr_volume + maxAmountToPour
 
     def canPour(self, source, destination):
+        """Determines whether or not there is room to pour"""
         if source.isEmpty():
             return False
         elif destination.isFull():
@@ -133,6 +141,7 @@ class assignment6:
             return True
 
     def isSolutionVertex(self, currentVertex):
+        """Determines whether the current vertex is part of the solution"""
         if self.goal_amount in currentVertex.getId():
             return True
         else:
@@ -153,8 +162,8 @@ class assignment6:
         while currentVertex.getPred() is not None:
             currentVertex = currentVertex.getPred()
             printList.insert(0, currentVertex.getId())
-        #for item in printList:
-         #   print(item)
+        # for item in printList:
+        #   print(item)
         return printList
 
 
@@ -166,34 +175,88 @@ class Container:
         self.max_volume = max_volume
 
     def fill(self):
+        """Fills the containers with the maximum amount possible"""
         self.curr_volume = self.max_volume
 
     def getCurrVolume(self):
+        """returns the volume of the container"""
         return self.curr_volume
 
     def getMaxVolume(self):
+        """Determines the maximum amount of volume in the container"""
         return self.max_volume
 
     def setCurrVolume(self, volume):
+
         self.curr_volume = volume
 
     def isFull(self):
+        """Determines if the container is full and whether or not can be poured into"""
         if self.curr_volume == self.max_volume:
             return True
         else:
             return False
 
     def isEmpty(self):
+        """Determines whether the container is empty and if it can be poured into"""
         if self.curr_volume == 0:
             return True
         else:
             return False
 
 
-if __name__ == '__main__':
+class TestHashTable(unittest.TestCase):
+    """Test Class to test some methods"""
+
+    def testFindSolution(self):
+        """Tests the FindSolution method"""
+        a1 = assignment6()
+        a2 = assignment6()
+        fsList = ['0,0', '3,0', '0,3', '3,3', '2,4']
+        self.assertEqual(a1.findsolution(3, 4, 2), fsList)
+        self.assertNotEqual(a2.findsolution(3, 4, 3), fsList)
+
+    def testGetEligibleStates(self):
+        """Tests the getEligibleStates method"""
+        a3 = assignment6()
+        a4 = assignment6()
+        gesList = ['3,0', '3,4', '0,3', '3,3', '0,4', '2,4', '3,1']
+        gesList2 = ['4,0', '3,0', '4,4', '0,4', '3,4', '0,3', '3,5', '3,1', '3,3', '2,4']
+        self.assertEqual(a3.getEligibleStates(3, 4, "3,0"), gesList)
+        self.assertNotEqual(a4.getEligibleStates(3, 4, "4,0"), gesList)
+        self.assertEqual(a4.getEligibleStates(3, 4, "4,0"), gesList2)
+        self.assertNotEqual(a4.getEligibleStates(3, 4, "5,3"), gesList2)
+
+
+def main():
+    """Main Method"""
     a6 = assignment6()
-    print(a6.findsolution(3, 4, 2))
-    print(a6.getEligibleStates(3, 4, "3,0"))
+    # print(a6.findsolution(3, 4, 2))
+    # print(a6.getEligibleStates(3, 4, "4,0"))
+    # a6.findsolution(3, 4, 2)
+    # a6.container_a = 3
+    # a6.container_b = 4
+    # a6.goal_amount = 2
+    print(a6.buildGraph2())
+    # g = Graph()
+    # g.addVertex("0,0")
+    # v0 = g.getVertex("0,0")
+    # v0.setDistance(0)
+    # g.addEdge("0,0", "3,0")
+    # v = g.getVertex("3,0")
+    # print(v.getId())
+    # v.setDistance(1)
+    # v.setPred(v0)
+    # print(g.getVertices())
+
+
+def unittest_main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
+    unittest_main()
 
     # g = Graph()
     # g.addVertex("0,0")
